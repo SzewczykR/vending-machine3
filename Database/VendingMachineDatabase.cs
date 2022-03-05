e6bbc328f54e9623373b22881610a2403b316b75
Ryszard Szewczyk <szewczykry@gmail.com>
2022-03-05 16:08:27 +0100

    db context

---
+++ Database/VendingMachineDatabase.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using vending_machine2.Models;
+
+namespace vending_machine2.Database
+{
+    public class VendingMachineDatabase : DbContext
+    {
+        public DbSet<VendingMachine> VendingMachines { get; set; }
+        public DbSet<Drink> Drinks { get; set; }
+        public DbSet<Cash> Cashes { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
