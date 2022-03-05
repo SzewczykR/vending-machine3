80bf6ab97cb4a5733d34d4abf56d654e9f276c80
Ryszard Szewczyk <szewczykry@gmail.com>
2022-03-05 15:59:40 +0100

    user

---
+++ User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vending_machine2
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; } 
+        public Cash Cash { get; set; }
+        public List<Drink> Drinks { get; set; } = new List<Drink>();
+    }
+}
